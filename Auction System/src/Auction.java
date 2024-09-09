78009d5f8004654861c05641d739812df49cca4a
Shivani-Kaveti <151118872+Shivani-Kaveti@users.noreply.github.com>
2024-09-09 23:00:58 +0530

    Add files via upload

---
+++ Auction System/src/Auction.java
@@ -0,0 +1,7 @@
+
+public interface Auction {
+	 void addObserver(Bidder bidder);
+	 void removeObserver(Bidder bidder);
+	 void notifyObservers(String message);
+
+}
---
+++ Auction System/src/Bidder.java
@@ -0,0 +1,5 @@
+
+public interface Bidder {
+	void update(String message);
+
+}
